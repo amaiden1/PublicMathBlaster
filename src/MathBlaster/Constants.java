73017bea3c7ab2a218e7712cbbac03a1e4a1cbbe
Nolan <nierardi@radford.edu>
2019-04-02 10:59:53 -0400

    added fatal block position constant

--- src/MathBlaster/Constants.java
+++ src/MathBlaster/Constants.java
@@ -14,7 +14,7 @@ public final class Constants {
 	 */
 
 	// Dev Mode
-	public static final boolean DEV_MODE = true;
+	public static final boolean DEV_MODE = false;
 
 	/*
 	 * Menu Constants
@@ -23,6 +23,14 @@ public final class Constants {
 		PAUSE_RESPONSE_CONTINUE = 0,
 		PAUSE_RESPONSE_MAIN_MENU = 1,
 		PAUSE_RESPONSE_QUIT = 2;
+
+	/*
+	 * Controller Constants
+	 */
+	public static final int
+		FATAL_BUTTON_DIST = 400
+
+		;
 
 	// Prevent instantiation of the class
 	private Constants() {}
